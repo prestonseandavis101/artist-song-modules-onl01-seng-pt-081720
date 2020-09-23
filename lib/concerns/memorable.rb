c61925a8a6c73993ff67ef8fc7cba62c421a043f
Preston Davis <prestonseandavis101@yahoo.com>
2020-09-23 01:05:47 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,9 +1,16 @@
 module Memorable
+  module ClassMethods
   def reset_all
-    all.clear
+    self.all.clear
   end
 
   def count
-    all.count
+    self.all.count
+  end
+end
+
+module InstanceMethods
+  def initialize
+    self.class.all << self
   end
 end
