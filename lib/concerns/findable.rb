fba19c146dd6a7694c137e300284c5430c2732a1
Preston Davis <prestonseandavis101@yahoo.com>
2020-09-23 00:46:11 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -2,6 +2,6 @@ module Findable
   
   def find_by_name(name)
     Artist.all.detect{|a| a.name == name}
-    Song.all.detect{|a| a.name == name}
+    Class.all.detect{|a| a.name == name}
   end
 end
