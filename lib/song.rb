adb6d4e98c09b351b3bd4d207fc97581fa760ede
Preston Davis <prestonseandavis101@yahoo.com>
2020-09-23 01:18:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,15 +4,13 @@ require_relative '../lib/concerns/paramble'
 
 require 'pry'
 
-class Song
- include Paramble 
- include Memorable::InstanceMethods
- extend Memorable::ClassMethods
+
  
- ####Memorable::ClassMethods
-  ##include
-  #Memorable::InstanceMethods
-  #include Paramble
+ extend 
+ Memorable::ClassMethods
+  include
+  Memorable::InstanceMethods
+  include Paramble
   
   attr_accessor :name
   attr_reader :artist
