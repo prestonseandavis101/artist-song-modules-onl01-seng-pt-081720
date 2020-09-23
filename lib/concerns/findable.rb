e8e270dff92ad644eaa9bda65603b3855fe00a6d
Preston Davis <prestonseandavis101@yahoo.com>
2020-09-23 00:48:41 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,7 +1,8 @@
 module Findable
   
   def find_by_name(name)
-    #Artist.all.detect{|a| a.name == name}
-    ObjectSpace.all.detect{|a| a.name == name}
+    Artist.all.detect{|a| a.name == name}
+  end
+    Song.all.detect{|a| a.name == name}
   end
 end
